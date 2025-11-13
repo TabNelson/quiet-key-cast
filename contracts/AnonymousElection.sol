2445d00f42910c7afcbcb101a0b65a8b556d622b
TabNelson <trestburghu5@outlook.com>
2025-11-12 16:58:01 -0800

    docs: improve error messages

--- contracts/AnonymousElection.sol
+++ contracts/AnonymousElection.sol
@@ -1,4 +1,4 @@
-// fix: correct encrypted vote sum calculation
+// fix: correct encrypted vote sum calculation  // improve error messages
 pragma solidity ^0.8.24;
 
 import {FHE, euint32, externalEuint32} from "@fhevm/solidity/lib/FHE.sol";
